5db4a3b06d69983ea91f1fe1129ba2f3c24abac6
Sebastien Gault <sgault.webdev@gmail.com>
2024-11-07 16:36:45 +0100

    feature: add default content to demo + build

--- demo/src/Demo.tsx
+++ demo/src/Demo.tsx
@@ -1,21 +1,36 @@
 import { useState } from "react";
 import { BlockType } from "../../src/definitions";
 import { BlockEditor } from "../../src";
+import defaultDemoContent from "./default-demo-content.json";
+
 import "../../src/sg-block-editor-default-theme.scss";
 
 const Demo: React.FC = () => {
 
-    const [content, setContent] = useState<BlockType[]>([]);
+    const [content, setContent] = useState<BlockType[]>(defaultDemoContent);
+
+    const codeReplacerFn = (_: string, value: any) => {
+        
+        if (typeof value === "string" && value.length > 26) {
+            return value.slice(0, 25) + '...';
+        }
+        return value;
+    }
 
     return <div>
         <h1>Editeur de Blocs 1.0.0</h1>
         <main>
             <div className="leftPanel">
-                <BlockEditor onChange={(value) => setContent(value)} />
+                <BlockEditor onChange={(value) => setContent(value)} data={defaultDemoContent}/>
             </div>
 
             <div className="rightPanel">
-                {JSON.stringify(content).replace(/,/g, ',\n ').replace(/{/g, '{\n ').replace(/}/g, '\n }')}
+                <pre>
+                    <code>
+                        {JSON.stringify(content, codeReplacerFn, 2)}
+                    </code>
+                </pre>
+                
                     
             </div>
         </main>
